4327b5cd4ceb1c78cf8e971af8c62653fdaff9f5
Simone Tarantino <questamailnonmiserveaniente@gmail.com>
2020-03-04 16:24:08 +0100

    index.php base

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+  include 'database.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -5,6 +9,27 @@
     <title>php-hotel-crud</title>
   </head>
   <body>
-
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Stanza</th>
+          <th>Piano</th>
+          <th></th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <!-- <td >id </td> -->
+          <!-- <td> numero stanza </td> -->
+          <!-- <td> piano stanza </td> -->
+          <td><a href="#">INFO STANZA</a></td>
+          <td><a href="#">UPDATE</a></td>
+          <td>ELIMINA</td>
+        </tr>
+      </tbody>
+    </table>
   </body>
 </html>
