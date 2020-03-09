f64f92c69ca1a1f9e56ebaf3d3dac77c11be542b
Simone Tarantino <questamailnonmiserveaniente@gmail.com>
2020-03-09 17:08:51 +0100

    Update inserito

--- update/server.php
+++ update/server.php
@@ -1,1 +1,56 @@
+<?php
+  include __DIR__ . '/../database.php';
+  include __DIR__ . '/../functions.php';
 
+  $idRoom = $_POST['id'];
+  $beds = $_POST['beds'];
+  $floor = $_POST['floor'];
+  $roomNumber = $_POST['room_number'];
+
+  // facciamo delle verifiche per vedere se l'utente ha compilato tutti i campi
+  if (empty($idRoom)) {
+    die('ID non inserito');
+  }
+  if (empty($beds)) {
+    die('Numero letti non inserito');
+  }
+  if (empty($floor)) {
+    die('Numero piano non inserito');
+  }
+  if (empty($roomNumber)) {
+    die('Numero stanza non inserito');
+  }
+  // eseguiamo un foreach su tutti i post per verificare che i valori inseriti siano dei numeri (considerato che ogni campo deve essere necessariamente compilato con un numero)
+  foreach ($_POST as $key => $value) {
+    if (intval($value) == 0) {
+      die("$key non è un numero");
+    }
+  }
+
+  $result = getById($conn, 'stanze', $idRoom);
+
+  // se i valori del risultato sono più di 0 (ovvero la l'id che ci viene passato esiste ed è assegnato ad una stanza)(utilizziamo count perché il return della funzione sopra ci darà un array)
+  if (count($result) > 0) {
+    // dato che stiamo facendo un injection utilizziamo i punti interrogativi come segnaposto nei campi dove ci saranno i nuovi valori che verranno aggiunti per evitare che il form venga rotto dall'inserimento di valori non autorizzati
+    $sql = "UPDATE `stanze` SET `room_number` = ?, `beds` = ?, `floor` = ?, `updated_at` = NOW() WHERE id = ?";
+    // prepariamo la statement sql
+    $stmt = $conn->prepare($sql);
+    // definiamo che i parametri che andremo a passare sono tutti integer (iiii = integer x 4), e sono, rispettivamente in ordine, le variabili dichiarate
+    $stmt->bind_param("iiii", $roomNumber, $beds, $floor, $idRoom);
+    // eseguiamo la statemenet
+    $stmt->execute();
+    // chiudiamo la chiamata
+    $conn->close();
+
+    // se la statement ottiene un risultato positivo
+    if ($stmt->affected_rows > 0) {
+      // veniamo reindirizzati alla pagina della nuova stanza
+      header("Location: $basePath/info/info.php?id=$idRoom");
+    } else {
+      echo 'KO';
+    }
+  }
+  // altrimenti la stanza non esiste
+  else {
+    die('La stanza non esiste');
+  }
---
+++ update/update.php
@@ -0,0 +1,47 @@
+<?php
+include __DIR__ . '/../database.php';
+include __DIR__ . '/../partials/header.php';
+
+if (!empty($_GET['id'])) {
+  $idRoom = $_GET['id'];
+}
+
+$sql = "SELECT * FROM `stanze` WHERE `id`='$idRoom'";
+
+$result = $conn->query($sql);
+
+if ($result && $result->num_rows > 0) {
+  $room = $result->fetch_assoc();
+}
+else {
+  die('ID non esistente');
+}
+?>
+<div class="container">
+  <div class="row">
+    <div class="col-4 mx-auto">
+      <form action="server.php" method="POST">
+        <div class="form-group">
+          <label for="room_number">Numero di Stanza</label>
+          <input class="form-control" type="text" name="room_number" value="<?php echo  $room['room_number'] ?>">
+        </div>
+        <div class="form-group">
+          <label for="floor">Piano</label>
+          <input class="form-control" type="text" name="floor" value="<?php echo  $room['floor'] ?>">
+        </div>
+        <div class="form-group">
+          <label for="beds">Numero di letti</label>
+          <input class="form-control" type="text" name="beds" value="<?php echo  $room['beds'] ?>">
+        </div>
+        <div class="form-group">
+          <input type="hidden" name="id" value="<?php echo  $room['id'] ?>">
+          <input class="form-control" class="btn btn-submit" type="submit" value="Salva">
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<?php
+include __DIR__ . '/../partials/footer.php'
+?>
